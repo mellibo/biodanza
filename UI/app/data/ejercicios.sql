61dfbd44a6cc82d460b561e51251df9cc24821c3
Marcos Mellibovsky <mellibo@arsoft.com.ar>
2017-08-03 19:51:54 -0300

    agregado de coleccion a ejercicio

--- UI/app/data/ejercicios.sql
+++ UI/app/data/ejercicios.sql
@@ -1,4 +1,4 @@
-SELECT  e.idEjercicio, e.nombre, e.idGrupo, replace(replace(ge.nombre,char(13),'<br/>'),'\r','<br/>') as grupo, replace(replace(e.detalle,char(13),'<br/>'),'\r','<br/>') detalle, 
+SELECT  e.idEjercicio, e.nombre, e.idGrupo, replace(replace(ge.nombre,char(13),'<br/>'),'\r','<br/>') as grupo, replace(replace(e.detalle,char(13),'<br/>'),'\r','<br/>') detalle, cm.nombre coleccion, 
     (SELECT  m.idMusica as 'idMusica', m.idColeccion as 'idColeccion', cm.Nombre  as 'coleccion', m.nroCd as 'nroCd', m.nroPista as 'nroPista', m.Nombre as 'nombre', m.interprete as 'interprete', m.duracion as 'duracion', m.archivo  as 'archivo', m.carpeta as 'carpeta'
         FROM MusicaEjercicio me 
     inner join Musica m on me.IdMusica = m.IdMusica
@@ -6,7 +6,8 @@ SELECT  e.idEjercicio, e.nombre, e.idGrupo, replace(replace(ge.nombre,char(13),'
     WHERE  me.IdEjercicio = e.IdEjercicio OR me.IdEjercicio is null  FOR JSON PATH) musicas
   FROM [dbo].[Ejercicios] e 
     inner join GrupoEjercicio ge on ge.IdGrupo = e.IdGrupo
+    inner join ColeccionMusica cm on cm.IdColeccion = e.IdColeccion
     and e.Nombre not IN (SELECT NomBreEjercicio FROM EquivalenciaEjercicios WHERE IdEjercicio < 1000)
   WHERE e.IdGrupo > 49
-  Order by idgrupo, nombre
+  Order by idgrupo, e.nombre
   FOR JSON PATH
