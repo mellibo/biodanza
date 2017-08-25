71cf7f2965952ebacac6a9fa12ac77ad8721a605
Marcos Mellibovsky <mellibo@arsoft.com.ar>
2017-08-25 11:41:07 -0300

    deploy.cmd

---
+++ deploy.cmd
@@ -0,0 +1,6 @@
+cd UI
+"c:\Program Files\7-Zip\7z" u "D:\Google Drive\Biodanza\biosoft\biosoft-sin musica-coleccion IBF-BSAS.zip" app biosoft.html -r -xr!musica -xr!musicas.js -xr!ejercicios.js
+"c:\Program Files\7-Zip\7z" u "D:\Google Drive\Biodanza\biosoft\biosoft-sin musica-coleccion IBF.zip" app biosoft.html -r -xr!musica -xr!musicas.js -xr!ejercicios.js
+"c:\Program Files\7-Zip\7z" u "D:\Google Drive\Biodanza\biosoft\biosoft-con musica-coleccion IBF.zip" app biosoft.html -r -xr!musica -xr!musicas.js -xr!ejercicios.js
+"c:\Program Files\7-Zip\7z" u "D:\Google Drive\Biodanza\biosoft\biosoft-con musica-coleccion IBF-BSAS.zip" app biosoft.html -r -xr!musica -xr!musicas.js -xr!ejercicios.js
+cd..
