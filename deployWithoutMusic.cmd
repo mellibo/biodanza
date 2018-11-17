9be5a886b901ed05a4d9a299233d5efbb72bcd41
Marcos Mellibovsky <mellibo@arsoft.com.ar>
2018-11-17 12:32:08 -0300

    deployCode cmd

---
+++ deployWithoutMusic.cmd
@@ -0,0 +1,4 @@
+del "D:\Google Drive\Biodanza\biosoft\biosoft.zip"
+cd UI
+"c:\Program Files\7-Zip\7z" a -tzip "D:\Google Drive\Biodanza\biosoft\biosoft.zip" -r *.html *.js *.xlsx *.xls *.eot *.svg *.ttf *.woff *.woff2 *.css *.map *.png *.jpg *.docx *.gif -x!BsAs_73 -x!BsAs_old  -x!Marcos  -x!CPAZ  -x!Test  -x!ejercicios.old.xlsx  -x!ejercicios*2018*.xlsx 
+cd..
