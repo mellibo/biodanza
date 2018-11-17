75216acf3314142c888d6939ab6556e29492c28e
Marcos Mellibovsky <mellibo@arsoft.com.ar>
2018-11-17 09:22:41 -0300

    busquedas rankeadas

--- UI/app/scripts/services.js
+++ UI/app/scripts/services.js
@@ -1,144 +1,9 @@
 ﻿var services = angular.module('services', []);
 
-//function addMusicasAEjercicio(ejercicio) {
-//    if (typeof ejercicio.musicas !== "undefined") return;
-//    ejercicio.musicas = [];
-//    angular.forEach(ejercicio.musicasId,
-//        function (value) {
-//            var musica;
-//            if (typeof value === "number") { // para cuando idMusica es numerico
-//                angular.forEach(db.musicas, function(item) {
-//                    if (item.oldId === value) {
-//                        musica = item;
-//                        ejercicio.musicas.push(musica);
-//                        return;
-//                    }
-//                });
-//            } else {
-//                musica = eval("db.musicas." + value);
-//                if (musica) ejercicio.musicas.push(musica);
-//            }
-//        });
-//}
-
 services.factory('contextService', ['$q', '$localStorage', '$uibModal', 'NgTableParams', '$filter', '$location', '$rootScope', 'loaderService', function ($q, $localStorage, $uibModal, NgTableParams, $filter, $location, $rootScope, loaderService) {
     var context = {};
 
     context.isMobileOrTablet = function () { return ismobile || istablet };
-
-
-    //context.exportMiMusica = function() {
-    //    var code = "if (typeof db === 'undefined') { db = {}; }\n\ndb.miMusica = [";
-
-    //    var coma = "";
-    //    angular.forEach(db.miMusica, function (item) {
-    //        if (item.idMusica === 0 || typeof item.idMusica === "undefined") setIdMusica(item);
-    //        code += coma +
-    //            "{ idMusica : " + item.idMusica + ", coleccion : '" + item.coleccion + "', nroCd : " + item.nroCd + ", nroPista : " + item.nroPista + ", nombre: '" + item.nombre + "', interprete : '" + item.interprete + "', duracion : '" + item.duracion + "', archivo : '" + item.archivo + "', carpeta : '" + item.carpeta + "', lineas : '" + item.lineas + "' }";
-    //    });
-    //    code += "];";
-    //    downloadString(code, "mimusica.js");
-    //}
-
-
-    //context.importarMusicas = function (file, cb) {
-    //    var result = [];
-    //    var reader = new FileReader();
-    //    reader.onloadend = function (evt) {
-    //        console.log("reader.onloadend");
-    //        if (evt.target.readyState === FileReader.DONE) {
-    //            var json="";
-    //            try {
-    //                json = eval(evt.target.result);
-    //            } catch (e) {
-    //            }
-    //            if (!Array.isArray(json)) {
-    //                result.push({ msg: "el archivo de importación es incorrecto." });
-    //                cb(result);
-    //                return;
-    //            }
-    //            var audio = new Audio();
-    //            audio.musicas = json;
-    //            audio.onerror = function (error) {
-    //                result.push({
-    //                    titulo: audio.musica.nombre,
-    //                    coleccion: audio.musica.coleccion,
-    //                    archivo: audio.musica.archivo,
-    //                    ok: false,
-    //                    msg: "error al cargar archivo: " + loaderService.config().pathMusica +
-    //                            audio.musica.coleccion +
-    //                            '/' +
-    //                            audio.musica.carpeta +
-    //                            '/' +
-    //                            audio.musica.archivo
-    //            });
-    //                console.log("error en archivo " + audio.musica.archivo);
-    //                if (audio.musicas.length === 0) {
-    //                    cb(result);
-    //                    return;
-    //                }
-    //                testMusica(audio);
-    //            }
-    //            audio.onplaying = function () {
-    //                var search = $filter('filter')(db.musicas,
-    //                    {
-    //                        nroCd: audio.musica.nroCd,
-    //                        nroPista: audio.musica.nroPista,
-    //                        coleccion: audio.musica.coleccion
-    //                    },
-    //                    true);
-    //                result.push({
-    //                    titulo: audio.musica.nombre,
-    //                    coleccion: audio.musica.coleccion,
-    //                    archivo: audio.musica.archivo,
-    //                    ok: search.length === 0,
-    //                    msg: search.length === 0 ? "importado correctamente" : "ya existe el título en la colección"
-    //            });
-    //                if (search.length === 0) {
-    //                    maxIdMusica++;
-    //                    audio.musica.idMusica = maxIdMusica;
-    //                    musicasPersonales.push(audio.musica);
-    //                    $localStorage.musicas = musicasPersonales;
-    //                    addMusica(audio.musica);
-    //                }
-    //                testMusica(audio);
-    //            }
-    //            if (audio.musicas.length === 0) {
-    //                cb(result);
-    //                return;
-    //            }
-    //            testMusica(audio);
-    //        }
-    //    };
-    //    reader.readAsText(file);
-    //};
-
-    /*
-    context.testMusica = function (audio) {
-        if (audio.musicas.length === 0) {
-            return;
-        }
-        var item = audio.musicas.pop();
-        var musica = context.nuevaMusica();
-        musica.coleccion = item.coleccion;
-        musica.nroCd = item.nroCd;
-        musica.nroPista = item.nroPista,
-            musica.nombre = item.titulo || "titulo incorrecto";
-        musica.interprete = item.interprete;
-        musica.duracion = item.duracion;
-        musica.archivo = item.archivo || "archivo incorrecto";
-        musica.carpeta = item.carpeta || "carpeta incorrecta";
-        musica.lineas = item.lineas;
-        audio.musica = musica;
-        audio.src = loaderService.config().pathMusica +
-            musica.coleccion +
-            '/' +
-            musica.carpeta +
-            '/' +
-            musica.archivo;
-        audio.play();
-    }
-    */
     return context;
 }]);
 
@@ -164,35 +29,7 @@ services.factory('modelEjerciciosService', ['$q', '$localStorage', '$uibModal', 
         {
             total: db.ejercicios ? db.ejercicios.length: 0,
             getData: function (params) {
-                var orderedData =  db.ejercicios;
-                if (service.buscar !== "" || service.grupo !== 'TODOS') {
-                    var searchStrings = service.buscar.toUpperCase().split(" ");
-                    for (var i = 0; i < searchStrings.length; i++) {
-                        searchStrings[i] = searchStrings[i].normalize('NFD').replace(/[\u0300-\u036f]/g, "").trim();
-                    }
-                    searchStrings = $filter('filter')(searchStrings, (obj) => { return obj !== "" });
-                    orderedData = $filter('filter')(orderedData,
-                        function (ejercicio, index, array) {
-                            if (service.grupo !== "TODOS" &&
-                                service.grupo !== ejercicio.grupo) return false;
-                            if (service.buscar === "") return true;
-                            var searchString = service.buscar.toUpperCase();
-                            for (var i = 0; i < searchStrings.length; i++) {
-                                if (ejercicio.nombreNormalized.indexOf(searchStrings[i]) !== -1) return true;
-                                if (ejercicio.grupoNormalized.indexOf(searchStrings[i]) !== -1) return true;
-                            }
-                            var ok = false;
-                            angular.forEach(ejercicio.musicas,
-                                function (musica) {
-                                    if (ok) return;
-                                    for (var i = 0; i < searchStrings.length; i++) {
-                                        if (musica.nombre.toUpperCase().indexOf(searchStrings[i]) > -1) ok = true;
-                                        if (musica.interprete.toUpperCase().indexOf(searchStrings[i]) > -1) ok = true;
-                                    }
-                                });
-                            return ok;
-                        });
-                }
+                var orderedData = buscarEjercicios();
                 var ejercicios = orderedData.slice((params
                         .page() -
                         1) *
@@ -204,6 +41,43 @@ services.factory('modelEjerciciosService', ['$q', '$localStorage', '$uibModal', 
             }
         })
     };
+
+    function buscarEjercicios() {
+        if (service.buscar === "" && service.grupo === 'TODOS') return db.ejercicios; 
+        var searchStrings = service.buscar.toUpperCase().split(" ");
+        var i;
+        for (i = 0; i < searchStrings.length; i++) {
+            searchStrings[i] = searchStrings[i].normalize('NFD').replace(/[\u0300-\u036f]/g, "").trim();
+        }
+        searchStrings = $filter('filter')(searchStrings, (obj) => { return obj !== "" });
+        var search = [];
+        for (var j = 0; j < db.ejercicios.length; j++) {
+            var ejercicio = db.ejercicios[j];
+            var rank = 0;
+            if (service.grupo !== "TODOS" &&
+                service.grupo !== ejercicio.grupo) continue;;
+            for (i = 0; i < searchStrings.length; i++) {
+                if (ejercicio.nombreNormalized.indexOf(searchStrings[i]) !== -1) rank++;
+                if (ejercicio.grupoNormalized.indexOf(searchStrings[i]) !== -1) rank++;
+                for (var l = 0; l < ejercicio.musicas.length; l++) {
+                    var musica = ejercicio.musicas[l];
+                    if (musica.nombre.toUpperCase().indexOf(searchStrings[i]) > -1) { rank++; break;}
+                    if (musica.interprete.toUpperCase().indexOf(searchStrings[i]) > -1) {
+                         rank++; break;
+                    }
+                }
+            }
+            if (rank > 0) search.push({ rank: rank, ejercicio: ejercicio });
+        }
+        search.sort((a, b) => { return b.rank - a.rank });
+        var result = [];
+        for (i = 0; i < search.length; i++) {
+            result.push(search[i].ejercicio);
+        }
+        return result;
+
+    }
+
     service.select = false;
     service.grupos = db.grupos;
     //service.ejercicios = db.ejercicios;
@@ -279,6 +153,57 @@ services.factory('modelEjerciciosService', ['$q', '$localStorage', '$uibModal', 
 
 services.factory('modelMusicaService', ['$q', '$localStorage', '$uibModal', 'NgTableParams', '$filter', 'playerService', 'loaderService',
 function ($q, $localStorage, $uibModal, NgTableParams, $filter, playerService, loaderService) {
+
+    function buscarMusicas(searchStringsEjercicio, filter) {
+        var search = [];
+        var i;
+        if (searchStringsEjercicio.length === 0) {
+            if ((!filter.coleccion ||
+                filter.coleccion.length === 0) &&
+                (!filter.nroCd ||
+                filter.nroCd.length === 0) &&
+                (!filter.nombre ||
+                filter.nombre.length === 0) &&
+                (!filter.lineas ||
+                filter.lineas.length === 0)) return db.musicas;
+        }
+        for (i = 0; i < db.musicas.length; i++) {
+            var musica = db.musicas[i];
+            var rank = 0;
+            for (var m = 0; m < searchStringsEjercicio.length; m++) {
+                for (var l = 0; l < musica.ejerciciosId.length; l++) {
+                    var ejercicio = loaderService.getEjercicioById(musica.ejerciciosId[l]);
+                    if (ejercicio.nombreNormalized.indexOf(searchStringsEjercicio[m]) !== -1) rank++;
+                    if (ejercicio.grupoNormalized.indexOf(searchStringsEjercicio[m]) !== -1) rank++;
+                }
+            }
+            if (filter.coleccion && filter.coleccion.length > 0) {
+                if (musica.coleccion.indexOf(filter.coleccion.toUpperCase()) !== -1) rank++;
+            }
+            if (filter.nroCd && filter.nroCd.length > 0) {
+                if (musica.cdPista.indexOf(filter.nroCd.toUpperCase()) !== -1) rank++;
+            }
+            if (filter.nombre && filter.nombre.length > 0) {
+                var searchStrings = filter.nombre.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").split(" ");
+                searchStrings = $filter('filter')(searchStrings, (obj) => { return obj !== "" });
+                for (var j = 0; j < searchStrings.length; j++) {
+                    if (musica.nombreNormalized.indexOf(searchStrings[j]) !== -1) rank++;
+                    if (musica.interpreteNormalized.indexOf(searchStrings[j]) !== -1) rank++;
+                }
+            }
+            if (filter.lineas && filter.lineas.length > 0) {
+                if (!musica.lineas || musica.lineas.indexOf(filter.lineas.toUpperCase()) !== -1) rank++;
+            }
+            if (rank > 0) search.push({ rank: rank, musica: musica });
+        }
+        search.sort((a, b) => { return b.rank - a.rank });
+        var result = [];
+        for (i = 0; i < search.length; i++) {
+            result.push(search[i].musica);
+        }
+        return result;
+    }
+
         var service = {
             ejercicios: db.ejercicios,
             //ejercicio: {},
@@ -307,61 +232,13 @@ function ($q, $localStorage, $uibModal, NgTableParams, $filter, playerService, l
             tableParams: new NgTableParams({ count: 15 },
             {
                 total: db.musicas.length,
-                getData: function(params) {
-                    var orderedData = db.musicas;
-                    if (service.ejercicioTextFilter) {
-                        var searchStrings = service.ejercicioTextFilter.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").match(/\w+|"[^"]+"/g);
-                        for (var l = 0; l < searchStrings.length; l++) {
-                            searchStrings[l] = searchStrings[l].replace(/\"/g, "");
-                        }
-                        var arrEjs = $filter('filter')(db.ejercicios, (obj) => {
-                            for (var k = 0; k < searchStrings.length; k++) {
-                                if (obj.nombreNormalized.indexOf(searchStrings[k]) !== -1 || obj.grupo.indexOf(searchStrings[k]) !== -1) {
-                                    return true;
-                                }
-                            }
-                            return false;
-                        });
-                        var arrFiltrado = [];
-                        for (var i = 0; i < arrEjs.length; i++) {
-                            var eje = arrEjs[i];
-                            var filtroMusicas = eje.musicas;
-                            var arrEjMusica = $filter('filter')(eje.musicas,
-                                function (value, index, array) {
-                                    if (!arrFiltrado.includes(value)) arrFiltrado.push(value);
-                                });
-                            //angular.extend(arrFiltrado, arrEjMusica);
-                        }
-                        orderedData = arrFiltrado;
+                getData: function (params) {
+                    var searchStrings = service.ejercicioTextFilter.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").match(/\w+|"[^"]+"/g);
+                    if (!searchStrings) searchStrings = [];
+                    for (var l = 0; l < searchStrings.length; l++) {
+                        searchStrings[l] = searchStrings[l].replace(/\"/g, "");
                     }
-                    orderedData = params.sorting ? $filter('orderBy')(orderedData, params.orderBy()) : orderedData;
-                    //orderedData = params.filter ? $filter('filter')(orderedData, params.filter()) : orderedData;
-                    var filter = params.filter();
-                    orderedData = params.filter ? $filter('filter')(orderedData, (obj) => {
-                        if (filter.coleccion && filter.coleccion.length > 0) {
-                            if (obj.coleccion.indexOf(filter.coleccion.toUpperCase()) === -1) return false;
-                        }
-                        if (filter.nroCd && filter.nroCd.length > 0) {
-                            if (obj.cdPista.indexOf(filter.nroCd.toUpperCase()) === -1) return false;
-                        }
-                        if (filter.nombre && filter.nombre.length > 0) {
-                            var searchStrings = filter.nombre.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").split(" ");
-                            searchStrings = $filter('filter')(searchStrings, (obj) => { return obj !== "" });
-                            var match = false;
-                            for (var j = 0; j < searchStrings.length; j++) {
-                                if (obj.nombreNormalized.indexOf(searchStrings[j]) !== -1 ||
-                                    obj.interpreteNormalized.indexOf(searchStrings[j]) !== -1) {
-                                    match = true;
-                                    break;
-                                }
-                            }
-                            if (!match) return false;
-                        }
-                        if (filter.lineas && filter.lineas.length > 0) {
-                            if (!obj.lineas || obj.lineas.indexOf(filter.lineas.toUpperCase()) === -1) return false;
-                        }
-                        return true;
-                    } ) : orderedData;
+                    var orderedData = buscarMusicas(searchStrings, params.filter());
                     var musicas = orderedData.slice((params
                             .page() -
                             1) *
