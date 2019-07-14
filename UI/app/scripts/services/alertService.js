1ec77cb7f0a1f0779d221c820683685f7867dfd8
Marcos Mellibovsky <mellibo@gmail.com>
2019-07-14 20:59:59 -0300

    fix alerts

--- UI/app/scripts/services/alertService.js
+++ UI/app/scripts/services/alertService.js
@@ -1,17 +1,32 @@
 ﻿services.factory('alertService', 
 ['$timeout', function ($timeout) {
-        function addAlert(alert) {
+    function addAlert(alert) {
+        if (!alert.type) alert.type = "info";
             service.alerts.push(alert);
             $timeout(() => {
                 var index = service.alerts.indexOf(alert);
                 service.alerts.splice(index, 1);
             },
                 10000);
+            safeApply();
         }
+
+        function safeApply(fn) {
+            var scope = angular.element(document.querySelector('#fileImport')).scope();
+            var phase = scope.$$phase;
+            if (phase === '$apply' || phase === '$digest') {
+                if (fn && (typeof (fn) === 'function')) {
+                    fn();
+                }
+            } else {
+                scope.$apply(fn);
+            }
+        }
+
         var service = {
             alerts: [],
             addAlert: function(type, msg) {
-                var alert = { type: type, msg: msg }
+                var alert = { type: type, msg: msg };
                 addAlert(alert);
             },
             addDangerAlert: function(msg) {
@@ -19,6 +34,7 @@
                 addAlert(alert);                
             },
             addInfoAlert: function (msg) {
+                var alert = { type: "info", msg: msg };
                 addAlert(alert);
             },
             closeAlert: function(index) {
